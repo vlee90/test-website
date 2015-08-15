bf86ee9c5c2ea759d367c9341fee68ad7b1bc9d0
Vincent Lee <lee.m.vincent@gmail.com>
2015-08-14 22:29:03 -0700

    Added link

--- javascript.js
+++ javascript.js
@@ -1,5 +1,15 @@
 var email;
 
-function storeEmail(email) {
+$(document).ready(function() {
+	$('#email-form').on("submit", function() {
+		var email = $('#email-address').val();
+		storeEmail(email);
+		return false;
+	});
+})
 
+$("p").append("HI");
+
+function storeEmail(email) {
+	alert("Thank you " + email + " for signing up.");
 }
